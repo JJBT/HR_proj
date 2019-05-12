08e8e24b9630bb3e7070eacf6d754b9412ecb889
JJBT <chernvld@gmail.com>
2019-05-12 03:15:34 +0300

    predict

--- predict.py
+++ predict.py
@@ -1,20 +1,24 @@
 import pickle
 import pandas as pd
+import json
 
 from sklearn.metrics import accuracy_score
 
 path_to_df = "data/non_prog_parsed.xlsx"
 df = pd.read_excel(path_to_df)
 
-path_to_models = ['catbst', 'rf_new', 'rf_new1', 'rf_new', 'xgb_new', 'xgb_new1', 'xgb_new1_XY', 'xgb_new_XY']
+path_to_models = ['catbst', 'rf_new', 'rf_new1', 'xgb_new', 'xgb_new1', 'xgb_new1_XY', 'xgb_new_XY']
 
+path_to_models = list(map(lambda x: 'models/' + x, path_to_models))
 
-features = ['career', 'educ', 'it_descr',
-            'it_group_prop', 'it_post_count', 'it_post_prop',
-            'site', 'tight_post']
-X = df.loc[:, features].values
 
 for name in path_to_models:
+
+    with open(name + '_descr.json', 'r') as file:
+        features = json.load(file)['features']
+
+    X = df.loc[:, features].drop('y', axis=1).values
+
     model = pickle.load(open(name, 'rb'))
     preds = model.predict(X)
     print('Model - ', name)
